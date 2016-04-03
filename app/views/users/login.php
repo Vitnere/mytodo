968220ed10cee0cbca62aa5152d59cd1018e0c3e
Vitnere <nemanjakolar@gmail.com>
2016-04-03 11:36:16 +0200

    CI_Controler not found, sys change to fix

--- app/views/users/login.php
+++ app/views/users/login.php
@@ -6,12 +6,30 @@
 *-->
 
 <h3>Login Form</h3>
-<!--atributi forme-->
+
+<?php if($this->session->userdata('logged_in')) : ?><!-- Ako sam logovan  -->
+
+    <!-- onda prikazi logout formu -->
+    <p>You are logged in as <?php echo $this->session->userdata('username'); ?></p><!-- poruka u logout formi -->
+    <!--Start Logout Form-->
+    <?php $attributes = array('id' => 'logout_form',
+        'class' => 'form-horizontal'); ?>
+    <?php echo form_open('users/logout',$attributes); ?>
+    <!--Submit Buttons-->
+    <?php $data = array("value" => "Logout",
+                        "name" => "submit",
+                        "class" => "btn btn-primary"); ?>
+    <?php echo form_submit($data); ?>
+    <?php echo form_close(); ?>
+
+<?php else : ?><!-- inace prikazi login formu -->
+
+    <!--atributi forme-->
 <?php $attributes = array('id' => 'login_form', 'class' =>'form-horizontal'); ?>
 
-<!-- otvaranje forme,gdje se zadaju dva parametra,
- prvi je lokacija tj usmeravanje na login funkciju
- drugi parametar je davanje attributes varjable-->
+    <!-- otvaranje forme,gdje se zadaju dva parametra,
+     prvi je lokacija tj usmeravanje na login funkciju
+     drugi parametar je davanje attributes varjable-->
 
 <?php echo form_open('users/login', $attributes); ?>
 
@@ -21,10 +39,10 @@
     //Vrijednosti u formi pohranjene u array koji prosleduje podatke u $data varjablu
     // koja je standardni dio Codeignitera:
     $data = array('name' => 'username',//polje name sa predefinisanom vrijednoscu 'username'
-                  'placeholder' => 'Enter Username',//polje sa predefenisanom vrijednoscu Enter username
-                   'style'      => 'width:90%',//sirina teksta 90% od polja u kojem se nalazi
-                   'value'      =>  set_value('username'));//spasava username vrijednostu u slucaju greske
-                                                            //pri unosu forme
+    'placeholder' => 'Enter Username',//polje sa predefenisanom vrijednoscu Enter username
+    'style'      => 'width:90%',//sirina teksta 90% od polja u kojem se nalazi
+    'value'      =>  set_value('username'));//spasava username vrijednostu u slucaju greske
+    //pri unosu forme
     ?>
 
     <?php echo form_input($data); ?><!-- komanda da CI prepozna associtave arrey i prosledi podatke iz njega
@@ -39,9 +57,9 @@
     //Vrijednosti u formi pohranjene u array koji prosleduje podatke u $data varjablu
     // koja je standardni dio Codeignitera:
     $data = array('name'        => 'password',//polje name sa predefinisanom vrijednoscu 'password'
-        'placeholder' => 'Enter Password',//polje sa predefenisanom vrijednoscu Enter Password
-        'style'      => 'width:90%',//sirina teksta 90% od polja u kojem se nalazi
-        'value'     =>  set_value('password'));//spasava password vrijednostu u slucaju greske
+    'placeholder' => 'Enter Password',//polje sa predefenisanom vrijednoscu Enter Password
+    'style'      => 'width:90%',//sirina teksta 90% od polja u kojem se nalazi
+    'value'     =>  set_value('password'));//spasava password vrijednostu u slucaju greske
     //pri unosu forme
     ?>
 
@@ -56,8 +74,8 @@
     //Vrijednosti u formi pohranjene u array koji prosleduje podatke u $data varjablu
     // koja je standardni dio Codeignitera:
     $data = array('name'        => 'submit',//dugme name sa predefinisanom vrijednoscu 'submit'
-                   'class'      => 'btn btn-primary',//klasa i bootstrap ime klase za primarno dugme
-                    'value'     =>  'Login'); //vrijednost Submit dugmeta,tj njegova labela
+    'class'      => 'btn btn-primary',//klasa i bootstrap ime klase za primarno dugme
+    'value'     =>  'Login'); //vrijednost Submit dugmeta,tj njegova labela
     //pri unosu forme
     ?>
 
@@ -68,3 +86,6 @@
 
 
 <?php echo form_close(); ?><!-- zatvarenje forme -->
+
+<?php endif; ?>
+
